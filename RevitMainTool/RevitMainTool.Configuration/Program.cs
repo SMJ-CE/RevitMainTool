61a1591a521722d11a5a42b7b5dc00cc50ec63ca
Eivind Egholm Vørmadal <eev_98@hotmail.com>
2023-12-29 14:02:47 +0100

    Created project for MSIX and made it somewhat work. Needs to be able to run custom action on install and uninstall.

--- RevitMainTool/RevitMainTool.Configuration/Program.cs
+++ RevitMainTool/RevitMainTool.Configuration/Program.cs
@@ -1,12 +1,27 @@
-﻿namespace RevitMainTool.Configuration
+﻿using Squirrel;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RevitMainTool.Configuration
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            if (args.Length != 1) return;
+            var cwd = Directory.GetCurrentDirectory();
+            var content = "test" + string.Join(", ", args);
+            
+            try {
+                File.WriteAllText("C:\\Users\\eev_9\\source\\repos\\SMJTools\\RevitMainTool\\RevitMainTool\\RevitMainTool.Configuration\\Releases\\log.txt", content);
 
-            new ManifestFactory().Create(args[0]);
+                new ManifestFactory().Create(cwd);
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(content);
+            Console.ReadLine();
         }
     }
 }
